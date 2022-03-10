82c2c6415f4960a0d59cf71eddc5a80d72bdf788
adis98 <aditya.ssr@gmail.com>
2022-03-10 21:25:47 +0100

    Adopt commit max final version + archipelago version_1

--- project/adopt_commit_max/src/main/scala/adopt_commit_max.scala
+++ project/adopt_commit_max/src/main/scala/adopt_commit_max.scala
@@ -4,19 +4,18 @@ import akka.util.Timeout
 
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-import scala.util.{Failure, Random, Success}
+import scala.concurrent.{Await, Future}
 import scala.util.control.Breaks.{break, breakable}
-import scala.util.matching.Regex
+import scala.util.{Failure, Random, Success}
 
 //TODO: Make the adopt commit max object
 class adopt_commit_max extends Actor{
   var A = new ArrayBuffer[Int]()
-  var B = new ArrayBuffer[String]()
+  var B = new ArrayBuffer[(String,Int)]()
   for(i <- 0 until Main.N){
     A += -1 //initial value indication empty
-    B += "" //empty string initially
+    B += (("",-1)) //empty string initially
   }
   var index: Int = -1
   def receive: PartialFunction[Any,Unit] = {
@@ -24,11 +23,14 @@ class adopt_commit_max extends Actor{
       //println("received",v)
       //println(sender().path.name)
       index = ind
-      println(B)
+      //println(B)
       //print("indexed")
       propose(v)
     }
   }
+
+
+
   def propose(v: Int): Unit ={
     A(index) = v
     var Sa : Set[Int] = Set()
@@ -37,73 +39,110 @@ class adopt_commit_max extends Actor{
     }
     val Sa_temp = Sa - (-1)
     if(Sa_temp.toList.length == 1){
-      B(index) = "commit_"+Sa_temp.toList(0).toString
+      B(index) = ("commit",Sa_temp.toList(0))
     }
     else{
-      B(index) = "adopt_"+Sa.max.toString
+      B(index) = ("adopt",Sa.max)
     }
-    var Sb : Set[String] = Set()
+    var Sb : Set[(String,Int)] = Set()
     for(i <- 0 until Main.N){
       Sb  = Sb + B(i)
     }
     //println(Sb)
+    val Sb_temp = (Sb - (("",-1))).toList
 
-    val commitPattern: Regex = "commit".r
-    val Sb_temp = Sb - ""
-    if(Sb_temp.toList.length == 1){
+    if(Sb_temp.length == 1){
       //println("ad_max_"+Sb_temp.toList(0))
       //println("replied")
-      sender() ! "ad_max_"+Sb_temp.toList(0)
+      sender() ! Sb_temp(0) //send the pair (control, value)
     }
     else{
       var done = false
       breakable{
-        for(i <- 0 until Sb_temp.toList.length){
-          commitPattern.findFirstMatchIn(Sb_temp.toList(i)) match{
-            case Some(_) => {
-              //println("replied")
-              sender() ! "ad_max_"+Sb_temp.toList(i)
-              done = true
-              break
-            }
-            case None =>
+        //for(i <-0 until List(Sb_temp.length)
+        for(i <- 0 until Sb_temp.length){
+          if(Sb_temp(i) == ("commit", Int)){
+            sender() ! Sb_temp(i)
+            done = true
+            break
           }
+          //done = true
+          //break
         }
         if(done == false){
           //println("replied")
-          sender() ! "ad_max_"+Sb.max
+          sender() ! maximum(Sb_temp)
         }
       }
     }
+  }
 
+  def maximum(Sb_temp: List[(String, Int)]): (String, Int) = {
+    var (maxControl,maxVal) = Sb_temp(0)
+    for (i <- 1 until Sb_temp.length) {
+      var (currControl,currVal) = Sb_temp(i)
+      if(currControl == maxControl && currVal > maxVal){
+        maxVal = currVal
+      }
+      else if(currControl == "commit" && maxControl == "adopt"){
+        maxControl = currControl
+        maxVal = currVal
+      }
+    }
+    (maxControl,maxVal)
   }
 }
 
 class Processor extends Actor{    // Extending actor trait
-  var value = self.path.name.toInt //can be changed
+  var valu = Random.nextInt(10) //can be changed
   var index = self.path.name.toInt
   def receive = {                 //  Receiving message
-    case "enable" => {
+    case ("enable",adp_index:Int) => {
       println("execution for node "+ self.path.name)
       //TODO: For now it just prints, but here the nodes would actually run archipelago
       implicit val timeout: Timeout = Timeout(100.seconds)
-      //ask(Main.node(i) , "disable").mapTo[Int]
-      val res = (Main.ad_max ? (value,index))
+      //archipelago(value, personal_index,register_index)
+      val res = archipelago(valu, index,adp_index)
       Await.result(res,Duration.Inf)
       res.onComplete{
-        case Success(resp) => println("got: "+resp)
+        case Success((control,vals)) => {
+          println("got: "+(control,vals))
+          if(control == "adopt"){
+            valu = vals.asInstanceOf[Int]
+            //println("value is ",valu)
+          }
+          else if(control == "commit"){
+            valu = vals.asInstanceOf[Int]
+          }
+        }
         case f @ Failure(exception) =>
           exception.printStackTrace()
           f
       }
       sender() ! 1
-
     }
-    case "disable" =>{
+    case ("disable",adp_index) =>{
       println("disabled node "+self.path.name)      // Disabled case
       sender() ! 1
     }
+    case "give_final" => {
+      sender() ! ("final",valu)
+    }
   }
+
+  def archipelago(value:Int, personal_index:Int,reg_index:Int): Future[Any] = {
+    implicit val timeout: Timeout = Timeout(100.seconds)
+    val res  = Main.shared_array_max(reg_index) ? (value,personal_index)
+    Await.result(res,Duration.Inf)
+    res.onComplete{
+      case Success((control,vals)) => println("got: "+(control,vals))
+      case f @ Failure(exception) =>
+        exception.printStackTrace()
+        f
+    }
+    res
+  }
+
 }
 
 class Puppeteer extends Actor{
@@ -112,6 +151,9 @@ class Puppeteer extends Actor{
     case "BeginSimulation" => {
       println("Right, I'll begin the simulation")
       beginSimulation()
+    }
+    case ("final",value:Int) => {
+      println("final value for processor "+sender().path.name+" = "+value.toString)
     }
   }
   def beginSimulation(): Unit ={
@@ -123,10 +165,10 @@ class Puppeteer extends Actor{
       val results = Array.ofDim[Future[Int]](Main.N) //expect responses from the enabled nodes alone on completion
       for(i <- 0 until Main.N){
         if(disabledNodes contains(i)){
-          results(i) = ask(Main.node(i) , "disable").mapTo[Int] //disable the node for this round
+          results(i) = ask(Main.node(i) , ("disable",rnd-1)).mapTo[Int] //disable the node for this round
         }
         else{
-          results(i) = ask(Main.node(i) , "enable").mapTo[Int]
+          results(i) = ask(Main.node(i) , ("enable",rnd-1)).mapTo[Int]
         }
       }
       val aggList = results.toList
@@ -134,13 +176,18 @@ class Puppeteer extends Actor{
       Await.result(futList,Duration.Inf)
       println("----------round done------------")
     }
+    for(i <- 0 until Main.N){
+      Main.node(i) ! "give_final"
+    }
   }
 }
 
 object Main{
-  val rounds = 2 //no. of rounds
-  val K = 1 //the max number of disabled processes
-  val N = 5 //the total number of nodes
+  val rounds = 3 //no. of rounds
+  val shared_commit_array_size = rounds
+  val shared_array_max = new Array[ActorRef](shared_commit_array_size) //the array of adopt commit max objects
+  val K = 15 //the max number of disabled processes
+  val N = 20 //the total number of nodes
   val node = new Array[ActorRef](N)
   val actorSystem = ActorSystem("ActorSystem");
   val puppeteer = actorSystem.actorOf(Props[Puppeteer])
@@ -149,9 +196,10 @@ object Main{
     for(i <- 0 until N){
       node(i) = actorSystem.actorOf(Props[Processor],i.toString)
     }
+    for(i <- 0 until shared_commit_array_size){
+      shared_array_max(i) = actorSystem.actorOf(Props[adopt_commit_max],"obj_"+i.toString)
+    }
     puppeteer ! "BeginSimulation"
-    //var actor = actorSystem.actorOf(Props[HelloAkka],"HelloAkka")        //Creating actor
-    //actor ! "Hello Akka"                                                // Sending messages by using !
-    //actor ! 100.52
+
   }
 }
